0a1933e6a512aa0525853affd958d54b69ab3ca8
Jack Hansen <jackahansen18@gmail.com>
2021-06-26 22:56:02 -0700

    It works, I think, but why would anyone use this

---
+++ NounLang.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <string.h>
+
+// brainf*ck interpreter
+void run(char* inputString) {
+    char array[30000] = {0};
+    char *ptr = array;
+
+    size_t i = 0;
+    size_t len = strlen(inputString);
+    size_t loop;
+
+    for (; i < len; i++) {
+        char instruction = inputString[i];
+
+        if (instruction == '>') {
+            ++ptr;
+        } else if (instruction == '<') {
+            --ptr;
+        } else if (instruction == '+') {
+            ++*ptr;
+        } else if (instruction == '-') {
+            --*ptr;
+        } else if (instruction == '.') {
+            putchar(*ptr);
+        } else if (instruction == ',') {
+            *ptr = getchar();
+        } else if (instruction == ']' && *ptr) {
+            loop = 1;
+            while (loop > 0) {
+                instruction = inputString[--i];
+                if (instruction == '[') {
+                    loop--;
+                } else if (instruction == ']') {
+                    loop++;
+                }
+            }
+        }
+    }
+}
+
+char* convertToBF() {
+
+    int output = system("python3 bfConverter.py > tmp.txt");
+
+    FILE *fp = fopen("tmp.txt", "r");
+    static char buffer[1000];
+    fread(buffer, 1001,1,fp);
+    fclose(fp);
+    return buffer;
+}
+
+
+int main() {
+    run(convertToBF());
+    return 0;
+}
---
+++ bfConverter.py
@@ -0,0 +1,29 @@
+import re
+
+with open("input.txt", "r") as f:
+    data = f.read()
+    f.close()
+
+out = ""
+
+keys = [">", "<", "+", "-", ".", ",", "[", "]"]
+
+current = 0
+currentPos = 2
+
+words = re.split(" |\n", data)
+
+for word in words:
+    word = word.lower()
+    if (word == "the"):
+        currentPos -= 1
+    elif (word == "a" or word == "an"):
+        current += 2 ** currentPos
+        currentPos -= 1
+
+    if (currentPos == -1):
+        currentPos = 2
+        out += keys[current]
+        current = 0
+
+print(out)
